88221e2f7c9b0f25aba8d4a9396673b783b9b8c8
6SEVANS <samevans.sje@gmail.com>
2025-06-13 03:32:07 +1200

    Created folder for javascript files. Reformed navbar and started working on the 'burger menu' for smaller screen sizes.

---
+++ src/scripts/navbar.js
@@ -0,0 +1,7 @@
+// Controls the javascript for the navbar.
+
+//Testing getting a response when the burger menu is clicked.
+const burgerMenu = document.querySelector('#burger-menu');
+burgerMenu.addEventListener('click', () => {
+    console.log("Test");
+})
