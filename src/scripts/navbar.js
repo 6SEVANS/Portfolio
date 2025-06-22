664f472de25dc8a64e4f2da9de94d2a5467c38a5
6SEVANS <samevans.sje@gmail.com>
2025-06-22 21:44:15 +1200

    Footer positioning fixed.  Projects page created. Minor changes made to index page.

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,7 +2,9 @@
 //Detects when the burger menu is clicked.
 const burgerMenu = document.querySelector('#burger-menu');
 const navbar = document.getElementById("navbar-links");
+const bodyWrapper = document.getElementById("body-wrapper");
 burgerMenu.addEventListener('click', () => {
     navbar.classList.toggle('active');
     burgerMenu.classList.toggle('active');
+    bodyWrapper.classList.toggle('active');
 });
